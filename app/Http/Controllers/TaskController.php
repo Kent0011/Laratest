3d6597cfb1b666a044ab92d27d52e44b297cb5d8
Kento Yamamoto <key7135g@gmail.com>
2025-04-07 14:26:43 +0900

    taskに関する基本設定

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Models\Task;
 class TaskController extends Controller
 {
     //
     public function index(int $id)
     {
         $folders = Folder::all();
+
+        $folder = Folder::find($id);
+
+        $tasks = Task::where('folder_id', $id)->get();
+
         return view('tasks/index', [
             'folders' => $folders,
-            'folder_id' => $id,
+            'folder_id' => $folder->id,
+            'tasks' => $tasks
         ]);
     }
 }
